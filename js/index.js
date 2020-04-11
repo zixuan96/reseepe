59f369a579890e66ae8b1480344fd93f365ae337
Zixuan <a769492361@gmail.com>
2020-04-10 23:02:10 -0400

    Delete tesing script

--- js/index.js
+++ js/index.js
@@ -59,16 +59,13 @@ function uploaded(file) {
 }
 
 function setup() {
-    //$("#rec").hide();
     canvas = createCanvas(300, 300);
     canvas.addClass("canvas");
-    //img = createImg(upimg, imageReady);
     $(".tag").hide();
     uploadBtn = createFileInput(uploaded);
     uploadBtn.addClass("upload-btn");
 }
 
-// Change the status when the model loads.
 function modelReady() {
     console.log("Model is ready!");
     status = true;
@@ -80,7 +77,6 @@ function imageReady() {
 }
 
 function gotResult(err, results) {
-    //document.getElementByClassName('uploadBtn').opacity="0";
     uploadBtn.hide();
     $(".tag").show();
 
@@ -90,52 +86,39 @@ function gotResult(err, results) {
     }
     console.log(results);
     objects = results;
-    //upimg.resize(400,400);
     image(upimg, 0, 0, 300, 300);
 
     for (let i = 0; i < objects.length; i++) {
         if (i == 0) {
             name[q] = objects[i].label;
-            //console.log(objects[i].label);
             num[q] = 1;
         } else if (i > 0 && objects[i].label != objects[i - 1].label) {
-            //console.log(objects[i].label);
             q++;
             name[q] = objects[i].label;
             num[q] = 1;
         } else if (i > 0 && objects[i].label == objects[i - 1].label) {
             num[q]++;
-            //num++;
         }
 
-        if (objects[i].label == "cat") {
-            console.log("1234444");
-        }
-        if (objects[i].label != "bowl") {
-            noFill();
-            strokeWeight(2);
-            stroke(255, 255, 255);
-            rect(
-                objects[i].x * width + a,
-                objects[i].y * height,
-                objects[i].w * width,
-                objects[i].h * height,
-                5,
-                5,
-                5,
-                5
-            );
-            strokeWeight(1);
-            stroke(0, 0, 0);
-            text(
-                objects[i].label +
-                    " " +
-                    nfc(objects[i].confidence * 100.0, 0) +
-                    "%",
-                objects[i].x * width + 5 + a,
-                objects[i].y * height + 15
-            );
-        }
+        noFill();
+        strokeWeight(2);
+        stroke(0, 0, 0);
+        rect(
+            objects[i].x * width + a,
+            objects[i].y * height,
+            objects[i].w * width,
+            objects[i].h * height,
+            5,
+            5,
+            5,
+            5
+        );
+        strokeWeight(1);
+        text(
+            objects[i].label,
+            objects[i].x * width + 5 + a,
+            objects[i].y * height + 15
+        );
     }
     for (i = 0; i <= q; i++) {
         document.getElementById("ing").innerHTML =
@@ -165,9 +148,6 @@ function gotResult(err, results) {
         "<br>Total: " +
         String(sum.toFixed(1)) +
         " cal";
-    //document.getElementById('ing').innerHTML = 'Fred Flinstone';
-    //console.log(String(num));
-    //text(objects[0].label + " " + nfc(objects[0].confidence * 100.0, 2) + "%");
 }
 
 function draw() {}
